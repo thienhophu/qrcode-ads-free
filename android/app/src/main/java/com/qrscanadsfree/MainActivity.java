c5c9670bae5cc5b7523fc3c1a2c3792df8c97377
Hieu Nguyen <dhieu0307@gmail.com>
2021-11-29 22:01:43 +0700

    change application ID

--- android/app/src/main/java/com/qrscanadsfree/MainActivity.java
+++ android/app/src/main/java/com/qrscanadsfree/MainActivity.java
@@ -1,4 +1,4 @@
-package com.qrcodescannerproject;
+package com.thienhophu.qrscanadsfree;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "qrcodeScannerProject";
+    return "qrscanadsfree";
   }
 }
